b6cacdce929b84a09febca624847864e5b5dca06
wsht <952587282@qq.com>
2018-04-11 14:15:38 +0800

    longest common prefix

---
+++ problems/no14_longest_common_prefix.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+)
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+
+	compare := strs[0]
+	end := len(compare)
+	for i := 0; i < len(compare); i++ {
+		if !comparePreFix(strs, i, compare[i]) {
+			end = i
+			break
+		}
+	}
+
+	return compare[0:end]
+}
+
+func comparePreFix(strs []string, index int, value byte) bool {
+	for i := 0; i < len(strs); i++ {
+		if index >= len(strs[i]) || value != strs[i][index] {
+			return false
+		}
+	}
+
+	return true
+}
+
+/**
+less line solution
+*/
+
+func longestCommonPrefix2(strs []string) string {
+	for idx := 0; len(strs) > 0; idx++ {
+		for i := 0; i < len(strs); i++ {
+			if idx >= len(strs[i]) || (i > 0 && strs[i][idx] != strs[i-1][idx]) {
+				return strs[0][0:idx]
+			}
+		}
+	}
+	return ""
+}
+
+func main() {
+	strs := []string{"aa", "a"}
+	fmt.Println(longestCommonPrefix2(strs))
+}
